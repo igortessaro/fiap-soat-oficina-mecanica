391cf944e305bc865ae69124048946654d4b9d3b
Igor Tessaro <igorstessaro57@gmail.com>
2025-06-17 16:01:01 -0300

    Add initial project structure for SmartMechanicalWorkshop API, Domain, and Infrastructure

---
+++ src/Fiap.Soat.SmartMechanicalWorkshop.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Fiap.Soat.SmartMechanicalWorkshop.Domain;
+
+public class Class1
+{
+
+}
---
+++ src/Fiap.Soat.SmartMechanicalWorkshop.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Fiap.Soat.SmartMechanicalWorkshop.Infrastructure;
+
+public class Class1
+{
+
+}
