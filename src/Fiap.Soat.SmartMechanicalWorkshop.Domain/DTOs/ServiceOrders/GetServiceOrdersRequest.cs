45eebed83e30d20b0f97ff1f2596e757f872f6e0
Igor Tessaro <igorstessaro57@gmail.com>
2025-08-02 16:32:58 -0300

    chore: adjust dotnet format

--- src/Fiap.Soat.SmartMechanicalWorkshop.Domain/DTOs/ServiceOrders/GetServiceOrdersRequest.cs
+++ src/Fiap.Soat.SmartMechanicalWorkshop.Domain/DTOs/ServiceOrders/GetServiceOrdersRequest.cs
@@ -2,6 +2,7 @@ using Fiap.Soat.SmartMechanicalWorkshop.Domain.Shared;
 using Fiap.Soat.SmartMechanicalWorkshop.Domain.ValueObjects;
 
 namespace Fiap.Soat.SmartMechanicalWorkshop.Domain.DTOs.ServiceOrders;
+
 public class GetServiceOrdersRequest : PaginatedRequest
 {
     public EServiceOrderStatus? Status { get; set; }
